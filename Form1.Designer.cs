f10754725c351a65bc1e22fcf7289c48408d4a1d
Fabricio Melo <fabriciosartin4@hotmail.com>
2022-08-16 17:54:34 +0100

    Initial functions and Variables set. Key event handlers configured( KeyUp and KeyDown)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,149 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ammoLabel = new System.Windows.Forms.Label();
+            this.killsLabel = new System.Windows.Forms.Label();
+            this.healthLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.zombie1 = new System.Windows.Forms.PictureBox();
+            this.zombie2 = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.zombie3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.zombie1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ammoLabel
+            // 
+            this.ammoLabel.AutoSize = true;
+            this.ammoLabel.Font = new System.Drawing.Font("Old English Text MT", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ammoLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.ammoLabel.Location = new System.Drawing.Point(35, 30);
+            this.ammoLabel.Name = "ammoLabel";
+            this.ammoLabel.Size = new System.Drawing.Size(102, 28);
+            this.ammoLabel.TabIndex = 0;
+            this.ammoLabel.Text = "Ammo: 0";
+            // 
+            // killsLabel
+            // 
+            this.killsLabel.AutoSize = true;
+            this.killsLabel.Font = new System.Drawing.Font("Old English Text MT", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.killsLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.killsLabel.Location = new System.Drawing.Point(350, 30);
+            this.killsLabel.Name = "killsLabel";
+            this.killsLabel.Size = new System.Drawing.Size(94, 28);
+            this.killsLabel.TabIndex = 1;
+            this.killsLabel.Text = "Kills: 0";
+            // 
+            // healthLabel
+            // 
+            this.healthLabel.AutoSize = true;
+            this.healthLabel.Font = new System.Drawing.Font("Old English Text MT", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.healthLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.healthLabel.Location = new System.Drawing.Point(660, 30);
+            this.healthLabel.Name = "healthLabel";
+            this.healthLabel.Size = new System.Drawing.Size(80, 28);
+            this.healthLabel.TabIndex = 2;
+            this.healthLabel.Text = "Health";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.progressBar1.Location = new System.Drawing.Point(746, 35);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(165, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // zombie1
+            // 
+            this.zombie1.Image = global::Zombie_Shooter.Properties.Resources.zdown;
+            this.zombie1.Location = new System.Drawing.Point(113, 149);
+            this.zombie1.Name = "zombie1";
+            this.zombie1.Size = new System.Drawing.Size(71, 71);
+            this.zombie1.TabIndex = 4;
+            this.zombie1.TabStop = false;
+            this.zombie1.Tag = "zombie";
+            // 
+            // zombie2
+            // 
+            this.zombie2.Image = global::Zombie_Shooter.Properties.Resources.zdown;
+            this.zombie2.Location = new System.Drawing.Point(709, 149);
+            this.zombie2.Name = "zombie2";
+            this.zombie2.Size = new System.Drawing.Size(71, 71);
+            this.zombie2.TabIndex = 5;
+            this.zombie2.TabStop = false;
+            this.zombie2.Tag = "zombie";
+            // 
+            // player1
+            // 
+            this.player1.Image = global::Zombie_Shooter.Properties.Resources.up;
+            this.player1.Location = new System.Drawing.Point(411, 308);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(71, 100);
+            this.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player1.TabIndex = 6;
+            this.player1.TabStop = false;
+            // 
+            // zombie3
+            // 
+            this.zombie3.Image = global::Zombie_Shooter.Properties.Resources.zup;
+            this.zombie3.Location = new System.Drawing.Point(411, 540);
+            this.zombie3.Name = "zombie3";
+            this.zombie3.Size = new System.Drawing.Size(71, 71);
+            this.zombie3.TabIndex = 7;
+            this.zombie3.TabStop = false;
+            this.zombie3.Tag = "zombie";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.gameEngine);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.ClientSize = new System.Drawing.Size(923, 666);
+            this.Controls.Add(this.zombie3);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.zombie2);
+            this.Controls.Add(this.zombie1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.healthLabel);
+            this.Controls.Add(this.killsLabel);
+            this.Controls.Add(this.ammoLabel);
+            this.Name = "Form1";
+            this.Text = "Kill or Die";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.zombie1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ammoLabel;
+        private System.Windows.Forms.Label killsLabel;
+        private System.Windows.Forms.Label healthLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox zombie1;
+        private System.Windows.Forms.PictureBox zombie2;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox zombie3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,136 @@ namespace Zombie_Shooter
         {
             InitializeComponent();
         }
+
+        bool goup; //This boolean will be used for the player to go up the screen
+        bool godown; //This boolean will be used for the player to go down the screen
+        bool goleft; //This boolean will be used for the player to go left the screen
+        bool goright; //This boolean will be used for the player to go right the screen
+
+        string facing = "up"; //this string is called facing and it will be used to guide the bullets
+        double playerHealth = 100; //This double variable is called player health
+
+        int speed = 10; //Speed of the player
+        int ammo = 10; //Ammount of ammo the player have at the start of the game
+        int zombieSpeed = 3; //Speed of the zombies
+        int score = 0; // Player's score achieved in the game
+        bool gameOver = false; //This boolean is false in the beginning and will be used when the game is finished
+
+        Random random = new Random();   //Instance of the 'random' class and will be used to create a random number for this game
+
+
+
+        
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (gameOver == true) return; //If the game is over then nothing will be executed in this event
+
+             // If the left key is pressed, then the code bellow will be ran 
+            if(e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+                facing = "left";
+                player1.Image= Properties.Resources.left;
+            }
+
+
+            // If the right key is pressed, then the code bellow will be ran 
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+                facing = "right";
+                player1.Image= Properties.Resources.right;
+
+            }
+
+            // If the up key is pressed, then the code bellow will be ran
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+                facing = "up";
+                player1.Image= Properties.Resources.up;
+
+            }
+
+            // If the down key is pressed, then the code bellow will be ran 
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+                facing = "down";
+                player1.Image = Properties.Resources.down;
+            }
+
+
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+
+            if (gameOver) return; //If the game is over then nothing will be executed in this event
+
+            // below is the key up selection for the left key
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false; // change the go left boolean to false
+            }
+
+            // below is the key up selection for the right key
+            if (e.KeyCode == Keys.Right)
+            {
+                goright= false; // change the go right boolean to false
+            }
+
+            // below is the key up selection for the up key
+            if(e.KeyCode == Keys.Up)
+            {
+                goup= false; // change the go up boolean to false
+            }
+
+            // below is the key up selection for the down key
+            if (e.KeyCode == Keys.Down)
+            {
+                goup = false; // change the go down boolean to false
+            }
+
+            //Below is the firing function, I will only include a KeyUp function for this because a KeyDown would allow the user to spam/hold space bar to fire.
+            if (e.KeyCode == Keys.Space && ammo > 0)
+            {
+                ammo --; //Everytime user shoots, ammo is reduced by 1
+                shoot(facing); // This will pass 'facing'(Up, Down, Lef or Right) as an argument for the 'shoot' function to fire the weapon that direction
+
+
+                if (ammo <= 1) //IF ammo is 1 or less
+                {
+                    dropAmmo(); //Involke the drop ammo function
+                }
+            }
+
+
+
+
+
+        }
+
+        private void gameEngine(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dropAmmo()
+        {
+
+        }
+
+        private void shoot()
+        {
+
+        }
+
+        private void makeZombies()
+        {
+
+        }
     }
 }
